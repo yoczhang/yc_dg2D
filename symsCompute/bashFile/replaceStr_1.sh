52425f67483ec9283ad505aa1e4394692030113b
yczhang <yoczhang@126.com>
2022-01-15 22:12:21 +0800

    update

--- symsCompute/bashFile/replaceStr_1.sh
+++ symsCompute/bashFile/replaceStr_1.sh
@@ -1,7 +1,7 @@
-sed -i "" "s/=/ = /g" /Users/yczhang/NutstoreSync/YCCODES/yc_dg2D/symsCompute/sysmDerivate_log.txt
-sed -i "" "s/0;/0*x; /g" /Users/yczhang/NutstoreSync/YCCODES/yc_dg2D/symsCompute/sysmDerivate_log.txt
-sed -i "" "s/*/.*/g" /Users/yczhang/NutstoreSync/YCCODES/yc_dg2D/symsCompute/sysmDerivate_log.txt
-sed -i "" "s/\^/.^/g" /Users/yczhang/NutstoreSync/YCCODES/yc_dg2D/symsCompute/sysmDerivate_log.txt
-sed -i "" "s/PI/pi/g" /Users/yczhang/NutstoreSync/YCCODES/yc_dg2D/symsCompute/sysmDerivate_log.txt
-sed -i "" "s/muu/mu/g" /Users/yczhang/NutstoreSync/YCCODES/yc_dg2D/symsCompute/sysmDerivate_log.txt
-sed -i "" "s/x,y)/x,y) /g" /Users/yczhang/NutstoreSync/YCCODES/yc_dg2D/symsCompute/sysmDerivate_log.txt
+sed -i "" "s/=/ = /g" /Users/yczhang/Documents/MatlabCode2Github/yc_dg2D/symsCompute/sysmDerivate_log.txt
+sed -i "" "s/0;/0*x; /g" /Users/yczhang/Documents/MatlabCode2Github/yc_dg2D/symsCompute/sysmDerivate_log.txt
+sed -i "" "s/*/.*/g" /Users/yczhang/Documents/MatlabCode2Github/yc_dg2D/symsCompute/sysmDerivate_log.txt
+sed -i "" "s/\^/.^/g" /Users/yczhang/Documents/MatlabCode2Github/yc_dg2D/symsCompute/sysmDerivate_log.txt
+sed -i "" "s/PI/pi/g" /Users/yczhang/Documents/MatlabCode2Github/yc_dg2D/symsCompute/sysmDerivate_log.txt
+sed -i "" "s/muu/mu/g" /Users/yczhang/Documents/MatlabCode2Github/yc_dg2D/symsCompute/sysmDerivate_log.txt
+sed -i "" "s/x,y)/x,y) /g" /Users/yczhang/Documents/MatlabCode2Github/yc_dg2D/symsCompute/sysmDerivate_log.txt
